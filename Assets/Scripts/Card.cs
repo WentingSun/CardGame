80d2bbed80a3bc62298a397f71b84c9301fb2722
WENTING SUN <45490316+WentingSun@users.noreply.github.com>
2023-02-02 23:28:18 +0000

    Adding some resource

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,11 @@
+public class Card
+{
+    public int id;
+    public string cardName;
+
+    public Card(int _id, string _cardName){
+        this.id = _id;
+        this.cardName = _cardName;
+        
+    }
+}
---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    public TextMeshPro nameText;
+    public Image cardImage;
+    public Card card;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ShowCard()
+    {
+        nameText.text = card.cardName;
+        
+    }
+}
